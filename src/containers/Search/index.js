e4ba7bea621fe916bb4bb81a9ffdb6758f5d3a77
teffcode <tefy.1@live.com>
2018-09-18 12:25:28 -0500

    Fix Search Component: responsive styles

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -29,13 +29,15 @@ class Search extends Component {
     return(
       <div className="search">
         <div className="search-header"></div>
-        <h1>• Search •</h1>
-        <SearchBar onSearchTerm={searchData}/>
-        {
-          Object.keys(tracks).length > 0 
+        <div className="search-content">
+          <h1>• Search •</h1>
+          <SearchBar onSearchTerm={searchData}/>
+          {
+            Object.keys(tracks).length > 0 
             ? <TracksTable history={history} tracks={tracks} />
             : null
-        }
+          }
+        </div>
       </div>
     );
   }
